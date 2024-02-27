06b3768f46d7bc1bdbf1e11263035e2bc237a4d1
Rayane Pimentel <37915359+rayanepimentel@users.noreply.github.com>
2024-02-27 12:58:25 -0300

    alterar url

--- site/public/header.js
+++ site/public/header.js
@@ -1,7 +1,6 @@
-// footer-element.js
 class Header extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML = `
+  connectedCallback() {
+      this.innerHTML = `
       <header>
         <div class="nav-container">
           <nav>
@@ -11,7 +10,17 @@ class Header extends HTMLElement {
           </nav>
         </div>
       </header>
-    `;
-    }
+      `;
+
+      // Adiciona um ouvinte de evento de clique a todos os links do menu
+      const menuLinks = this.querySelectorAll('.menu-link');
+      menuLinks.forEach(link => {
+          link.addEventListener('click', event => {
+              event.preventDefault(); // Prevenir o comportamento padrão de seguir o link
+              const href = link.getAttribute('href');
+              window.history.pushState({}, '', href); // Altera a URL sem recarregar a página
+          });
+      });
+  }
 }
 customElements.define("meu-header", Header);
