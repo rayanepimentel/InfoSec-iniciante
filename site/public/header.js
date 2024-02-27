24d41e2bb5e6e404d45ecf420b6ef3904dae9aab
Rayane Pimentel <37915359+rayanepimentel@users.noreply.github.com>
2024-02-27 12:13:16 -0300

    add site

---
+++ site/public/header.js
@@ -0,0 +1,16 @@
+// footer-element.js
+class Header extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+      <header>
+        <div class="nav-container">
+          <nav>
+            <div class="logo"></div><a href="/" style="font-size: xx-large;">&#x1F47E;</a>
+            <div class="nav-bar"><a href="../../README.md">Início_</a><a href="../cronograma/cronograma.html">Cronograma_</a><a href="">WhereAmI_</a><a href="">Técnicas e mais_</a></div>
+          </nav>
+        </div>
+      </header>
+    `;
+    }
+}
+customElements.define("meu-header", Header);
