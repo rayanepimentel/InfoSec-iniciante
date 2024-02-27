b8307bec7b40c007e1b13d338e42789b145079af
Rayane Pimentel <37915359+rayanepimentel@users.noreply.github.com>
2024-02-27 12:19:02 -0300

    alterando pag inicio

--- site/public/header.js
+++ site/public/header.js
@@ -6,7 +6,7 @@ class Header extends HTMLElement {
         <div class="nav-container">
           <nav>
             <div class="logo"></div><a href="/" style="font-size: xx-large;">&#x1F47E;</a>
-            <div class="nav-bar"><a href="../../README.md">Início_</a><a href="../cronograma/cronograma.html">Cronograma_</a><a href="">WhereAmI_</a><a href="">Técnicas e mais_</a></div>
+            <div class="nav-bar"><a href="../../index.html">Início_</a><a href="../cronograma/cronograma.html">Cronograma_</a><a href="">WhereAmI_</a><a href="">Técnicas e mais_</a></div>
           </nav>
         </div>
       </header>
